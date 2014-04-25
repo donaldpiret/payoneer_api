204ad8b56d89a4bc030987f41985165be3e587e7
Donald Piret <donald@donaldpiret.com>
2014-04-25 17:10:12 +0800

    Add tentative support for info prefil

--- lib/payoneer_api/client.rb
+++ lib/payoneer_api/client.rb
@@ -19,8 +19,12 @@ module PayoneerApi
     end
 
     def payee_link(member_name)
-      @member_name = member_name
-      result = get_api_call(payee_link_args)
+      result = get_api_call(payee_link_args(payee_id: member_name))
+      api_result(result)
+    end
+
+    def payee_prefill_link(member_name, attributes = {})
+      result = post_api_call(payee_prefill_args(attributes.merge(payee_id: member_name)))
       api_result(result)
     end
 
@@ -59,14 +63,72 @@ module PayoneerApi
       http.request(request).body
     end
 
-    def payee_link_args
+    def post_api_call(args_hash)
+      uri = URI.parse(api_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.set_form_data(args_hash)
+      http.request(request).body
+    end
+
+    def payee_link_args(args)
       {
         mname: 'GetToken',
         p1: @username,
         p2: @password,
         p3: @partner_id,
-        p4: @member_name
+        p4: args[:payee_id],
+        p5: args[:session_id],
+      }.delete_if { |_, value| value.nil? || value.empty? }
+    end
+
+    def payee_prefill_args(args)
+      {
+        mname: 'GetToken',
+        p1: @username,
+        p2: @password,
+        p3: @partner_id,
+        p4: args[:payee_id],
+        Xml: prefill_xml_data(args)
       }
+    end
+
+    def prefill_xml_data(args)
+      builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+        xml.PayoneerDetails do
+          xml.Details do
+            xml.prid @partner_id
+            xml.apuid args[:payee_id]
+            xml.sessionid args[:session_id] if args[:session_id]
+            xml.redirect args[:redirect_url] if args[:redirect_url]
+            xml.redirectTime args[:redirect_time] if args[:redirect_time]
+            xml.cardType args[:card_type] if args[:card_type]
+            xml.BlockType args[:block_type] if args[:block_type]
+            xml.PayoutMethodList (args[:payout_methods].is_a?(Array) ?
+              args[:payout_methods].join(',') :
+              args[:payout_methods]) if args[:payout_methods]
+            xml.RegMode args[:registration_mode] if args[:registration_mode]
+            xml.HoldApproval args[:hold_approval] if args[:hold_approval]
+          end
+          xml.PersonalDetails do
+            xml.firstName args[:first_name] if args[:first_name]
+            xml.lastName args[:last_name] if args[:last_name]
+            xml.dateOfBirth args[:date_of_birth] if args[:date_of_birth]
+            xml.address1 args[:address].is_a?(Array) ? args[:address].first : args[:address]
+            xml.address2 args[:address].last if args[:address].is_a?(Array)
+            xml.city args[:city] if args[:city]
+            xml.country args[:country_code] if args[:country_code]
+            xml.state args[:state_code] if args[:state_code]
+            xml.zipCode args[:zip_code] if args[:zip_code]
+            xml.mobile args[:mobile_phone] if args[:mobile_phone]
+            xml.phone args[:phone] if args[:phone]
+            xml.email args[:email] if args[:email]
+          end
+        end
+      end
+      builder.to_xml
     end
 
     def transfer_funds_args(options)
--- payoneer_api.gemspec
+++ payoneer_api.gemspec
@@ -2,10 +2,14 @@ Gem::Specification.new do |s|
   s.name        = 'payoneer_api'
   s.version     = '0.0.1'
   s.date        = '2013-05-30'
-  s.summary     = "Ruby wrapper for Payoneer API"
-  s.description = "Allows to easily generate the links for Ioneer registration"
-  s.authors     = ["Donald Piret"]
+  s.summary     = 'Ruby wrapper for Payoneer API'
+  s.description = 'Allows to easily generate the links for Pioneer registration'
+  s.authors     = ['Donald Piret']
   s.email       = 'ejabberd@gmail.com'
-  s.files       = ["lib/payoneer_api/exception.rb"]
+  s.files       = ['lib/payoneer_api/client.rb', 'lib/payoneer_api/payoneer_exception.rb']
   s.homepage    = 'https://github.com/roomorama/payoneer_api'
+
+  s.add_dependency 'nokogiri'
+
+  s.add_development_dependency 'rspec'
 end
